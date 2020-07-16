5a2566de15914daf11abeeb98a0ef690fc380b2a
gxe1157 <gx1157@yahoo.com>
2020-07-15 23:47:40 -0400

    Update multiple_entries_ver2.py
    Completely rewritten

--- multiple_entries_ver2.py
+++ multiple_entries_ver2.py
@@ -26,90 +26,84 @@ my_labels = []
 
 def something():
 	entry_list = ''
+	my_entries[0].insert(0,'text')
 	print(my_entries[0].get())
-	for entries in my_entries:
-		entry_list = entry_list + str(entries.get()) + '\n'
-	my_label.config(text=entry_list)
+	# print(dir(my_entries[0]))
+	# print(dir(list))	
+
+	for e, entries in enumerate(my_entries):
+		entry_list += f"{e} {entries.get()} \n"
+
+	print(entry_list)
+   
+	# my_label.config(text=entry_list)
+
+def show_ward( r1, r2, ward_no):
+	# v[id, form_no, ward, district, Municipalit]
+	my_label = Label(root, width=10, text= f"Ward {ward_no}", fg="white", bg="grey")
+	my_label.grid(row=r1, column=0, pady=1, padx=40)
+	my_label = Label(root, width=10, text= f"Total: 0")
+	my_label.grid(row=r2, column=0, pady=1, padx=40)
 
 
-def show_districts(mwd):
-	ward_no=''
-	mwds =[]
-	temp_values=[]
-	for k, v in mwd.items():
-		if ward_no == '': ward_no = v[2]
-
-		temp_values.append((v[3], v[2], v[0]))
-		if ward_no != v[2]:
-			ward_no = v[2] 						
-			temp_values.pop()			
-			# add last value to mwds
-			mwds.append( temp_values.pop() )
-			temp_values=[]
-		print(k, v)
-	mwds.append( temp_values.pop() )
-
-	my_header.config(text=f"MUNICIPALITY: {v[4]}", font=("helvetica", 14))
-	print(mwds)
-
-	offset_y = 0
+def show_districts(mwds):
+	# y=True
+	x = 1
+	y1 = 1
+	y2 = 2
+	offset = 2
+	ward_no = mwds[0][2]
 
 	for i, item in enumerate(mwds):
-		print( f"{1}  mwd: {item} offset_y: {offset_y}" )
+		item = ['' if x is None else x for x in item]
 
-'''
-			# Display Ward 
-			if y==1:
-				my_label = Label(root, width=10, text= f"Ward {ward_no}   [{mwd}]", fg="white", bg="grey")
-				my_label.grid(row=y1, column=0, pady=1, padx=40)
-				my_label = Label(root, width=10, text= f"Total: {mwd_id}")
-				my_label.grid(row=y1+1, column=0, pady=1, padx=40)
+		show_ward_no = True
+		if i == 0:
+			show_ward(y1, y2, item[2])			
 
-			if exit_loop == True: break
-			for x in range(1,9):
-				my_label = Label(root, width=10, text= f"District {xx}", fg="white", bg="grey")
-				my_label.grid(row=y1, column=x, pady=1, padx=5)
-				my_entry = Entry(root, width=10)	 
-				my_entry.grid(row=y2, column=x, pady=5, padx=5)
+		if ward_no != item[2]:
+			ward_no = item[2]
+			show_ward_no = False
+			x=9
 
-				# form data entry
-				my_labels.append(my_label)
-				my_entries.append(my_entry)
-				xx +=1
-				if xx > mwd:
-					exit_loop = True
-					break
-			# Add extra offset for more than 2 line of district boxes		
-			if y > 1: offset_y += y
-			# print(f"Position: y {y} {i}")
-'''
+		if x == 9:
+			x=1
+			if 	show_ward_no == True and len(ward_no)>0:
+				show_ward(y1, y2, item[2])						
+			y1 += offset
+			y2 += offset
 
+		my_label = Label(root, width=10, text= f"District {item[3]}", fg="white", bg="grey")
+		my_label.grid(row=y1, column=x, pady=1, padx=5)
+		my_entry = Entry(root, width=10)	 
+		my_entry.grid(row=y2, column=x, pady=5, padx=5)
+
+		# form data entry
+		my_labels.append(my_label)
+		if item[1] == '':
+			my_entry.config({"background": "#DEDCDC"})			
+
+		my_entry.insert(0, f"{item[1]}")
+		my_entries.append(my_entry)
+
+
+		x += 1
+
+	my_header.config(text=f"MUNICIPALITY: {item[4]}", font=("helvetica", 14))
 
 my_header = Label(root, text='MUNICIPALITY:', font=("helvetica", 14) )
 my_header.grid(row=0, column=0, columnspan=9, pady=20)
-
-my_label = Label(root, text='')
-my_label.grid(row=30, column=0, pady=20)
 
 my_button = Button(root, text="Click Me!", command=something)
 my_button.grid(row=90, column=0, pady=20)		
 
 muni ='TRENTON'
-muni = 'EAST WINDSOR'
-muni = 'HAMILTON'
+# muni = 'EAST WINDSOR'
+# muni = 'HAMILTON'
 
 query = "SELECT id, form_no, ward, district, Municipality FROM form_master WHERE municipality ='"+muni+"' ORDER BY municipality, ward, district"
 cursor.execute(query)
 rows = cursor.fetchall()
-# print(len(rows))
-
-mwd={}
-idx=''
-for row in rows:
-	idx = f"{row[2]}-{row[3]}"
-	mwd[idx]= row
-	# print(f"{row[2]}-{row[3]}")	
-
-show_districts(mwd)
+show_districts(rows)
 
 root.mainloop()
